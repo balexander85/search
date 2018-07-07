b5d152fa14b7bd08edbb5a3c86f2083edfd35a87
Brian Alexander <balexander04@gmail.com>
2018-07-07 23:20:18 +0900

    Initial commit, working as of today.

---
+++ search.py
@@ -0,0 +1,200 @@
+"""Search the web for items and get magnet url for Transmission"""
+from datetime import datetime
+from typing import List
+
+from requests_html import HTML, Element
+from util import (
+    LOGGER,
+    get_first_element,
+    get_user_input,
+    format_comments,
+    format_file_size,
+    format_file_size_to_int,
+    parse_comments,
+    print_border,
+    RequestsHtmlWrapper,
+)
+
+
+class Result:
+    """Create Search Result object with a parsed dictionary."""
+
+    def __init__(self, obj: dict):
+        self.name: str = obj.get('name')
+        self.media_type: str = obj.get('media_type', '')
+        self.category: str = obj.get('category', '')
+        self.seeders: int = int(obj['seeders'])
+        self.leachers: int = int(obj['leachers'])
+        self.file_size: str = format_file_size(obj.get('file_size', ''))
+        self.file_size_int: int = format_file_size_to_int(self.file_size)
+        self.vip_status: bool = True if obj.get('vip_status') else False
+        self.trusted: bool = True if obj.get('trusted') else False
+        self.href: str = obj.get('href')
+        self.comment_count: int = int(obj.get('comment_count'))
+        self.comments_section: List[str] = obj.get('comments_section', [])
+        self.uploader: str = obj.get('uploader')
+        self.magnet_link: str = obj.get('magnet_link')
+
+    def __str__(self) -> str:
+        """ Return str(self). """
+        return(
+            f'{self.name} | Size: {self.file_size} | Seed: {self.seeders} '
+            f'| Leach: {self.leachers} | Trusted: {self.trusted} |'
+            f' VIP: {self.vip_status} | Uploader: {self.uploader}\n'
+            f'    ML: {self.magnet_link}   \nComments: {self.comments_section}'
+        )
+
+    def __repr__(self) -> str:
+        """ Return repr(self). """
+        return f'{self.name}'
+
+
+class BaseSite:
+    """Parent class for searching websites."""
+
+    SEARCH_URL_PATTERN = '/{}'
+    WHITESPACE_PATTERN = '+'
+
+    def __init__(
+            self,
+            protocol: str = 'http',
+            domain: str = 'example',
+            tdl: str = 'com',
+            query: str = None
+    ):
+        """Construct URL with these base attributes.
+
+        :param protocol: Protocol used for website.
+        :param domain: Name of the domain where the web server belongs.
+        :param tdl: Top Level Domain for website.
+        :param query: User's search query
+        """
+        print_border(self.__class__.__name__)
+        self.base_url: str = f'{protocol}://{domain}.{tdl}'
+        self.query: str = query
+        self.request_wrapper: RequestsHtmlWrapper = RequestsHtmlWrapper(
+            url=self.search_url
+        )
+        self.html: HTML = self.request_wrapper.html
+
+    def __iter__(self) -> Result:
+        """Iterate through each result found from search query."""
+        for result in self.results:
+            yield result
+
+    @property
+    def search_url(self) -> str:
+        """Create search url with patch and whitespace replacement character"""
+        return self.base_url + self.SEARCH_URL_PATTERN.format(
+            self.query.replace(' ', self.WHITESPACE_PATTERN)
+        )
+
+    @property
+    def results(self) -> List[dict]:
+        """Abstract property which returns a list of results from search.
+
+        Note: Will raise error if not implemented in subclass.
+        """
+        raise NotImplementedError(
+            "Subclasses that use BaseSite must implement results property."
+        )
+
+
+class ThePirateBay(BaseSite):
+    """Subclass for searching Thepiratebay domain."""
+
+    SEARCH_URL_PATTERN = '/search/{}/0/99/0'
+    WHITESPACE_PATTERN = '%20'
+    RESULTS_TABLE = 'table#searchResult'
+    RESULTS_ROW = 'div.detName'
+    ITEM_NAME = 'div.detName'
+    ITEM_MAGNET_LINK = 'a[href^="magnet"]'
+    ITEM_COMMENTS = 'This torrent has {} comments.'
+    MEDIA_AND_CATEGORY_TYPE = 'td.vertTh'
+    SEEDERS_AND_LEACHERS = 'td[@align="right"]'
+    UPLOADER_VIP = 'img[@alt="VIP"]'
+    UPLOADER_TRUSTED = 'img[@alt="Trusted"]'
+    PARSED_COMMENTS = 'div.comment'
+
+    def __init__(
+            self,
+            protocol: str = "https",
+            domain: str = "thepiratebay",
+            tdl: str = "org",
+            query: str = None
+    ):
+        BaseSite.__init__(
+            self, protocol=protocol, domain=domain, tdl=tdl, query=query
+        )
+        self.results_table: Element = get_first_element(
+            html=self.html,
+            locator=self.RESULTS_TABLE
+        )
+
+    def parse_row(self, obj: Element) -> dict:
+        """Parse object for item information."""
+        return {
+            'name': get_first_element(obj, self.ITEM_NAME).text,
+            'media_type': get_first_element(
+                obj, self.MEDIA_AND_CATEGORY_TYPE).find('a')[0].text,
+            'category': get_first_element(
+                obj, self.MEDIA_AND_CATEGORY_TYPE).find('a')[1].text,
+            'seeders': obj.find(self.SEEDERS_AND_LEACHERS)[0].text,
+            'leachers': obj.find(self.SEEDERS_AND_LEACHERS)[1].text,
+            'magnet_link': get_first_element(
+                obj, self.ITEM_MAGNET_LINK).links.pop(),
+            'file_size': get_first_element(
+                obj, 'font.detDesc').text.split(',')[1],
+            'vip_status': get_first_element(obj, self.UPLOADER_VIP),
+            'trusted': get_first_element(obj, self.UPLOADER_TRUSTED),
+            'href': get_first_element(obj, 'a.detLink').links.pop(),
+            'uploader': (
+                obj.find('font.detDesc')[0].text.split('by')[-1].strip()
+            ),
+            'comment_count': (
+                0 if not obj.search(self.ITEM_COMMENTS)
+                else obj.search(self.ITEM_COMMENTS).fixed[0]
+            ),
+        }
+
+    def fetch_comments(self, results: List[Result]) -> List[Result]:
+        """Return a list of comments for a result if available."""
+        for result in results:
+            if result.comment_count >= 1:
+                page_url: str = f'{self.base_url}{result.href}'
+                page = self.request_wrapper.get_page(page_url=page_url)
+                comments = parse_comments(
+                    html=page, locator=self.PARSED_COMMENTS
+                )
+                if comments:
+                    result.comments_section = format_comments(
+                        [c.text.replace('\n', '') for c in comments]
+                    )
+        return results
+
+    @property
+    def results(self) -> List[Result]:
+        """Returning results from this site."""
+        if self.results_table:
+            results: List[Result] = [
+                Result(self.parse_row(row))
+                for row in self.results_table.find("tr")
+                if row.find(self.RESULTS_ROW)
+            ]
+            if results:
+                return self.fetch_comments(results)
+            return results
+
+
+if __name__ == '__main__':
+    user_input = get_user_input()
+    LOGGER.info(f'Searching for "{user_input}"')
+    start_time = datetime.now()
+
+    site_results = ThePirateBay(query=user_input)
+    for counter, site_result in enumerate(site_results, 1):
+        print(f'{counter}) {site_result}')
+
+    LOGGER.info(
+        f'Search duration {datetime.now() - start_time}'
+    )
---
+++ util.py
@@ -0,0 +1,138 @@
+"""Functions to be used by search module
+but not necessarily related to the searching itself.
+"""
+from logging import basicConfig, getLogger, INFO
+from re import match as regex_match
+from sys import argv, stdout
+from typing import List, Union
+
+from humanfriendly import parse_size
+from requests_html import Element, HTML, HTMLResponse, HTMLSession
+from retrying import retry
+
+# Setting up logger
+basicConfig(
+    level=INFO,
+    format="%(levelname)7s: %(message)s",
+    stream=stdout,
+)
+LOGGER = getLogger("Search Logger")
+
+
+def format_comments(comments: List[str]) -> List[str]:
+    """Add #.) to each comment"""
+    return [f'{i}.) {comment}' for i, comment in enumerate(comments, 1)]
+
+
+def format_file_size(size: str) -> str:
+    """Returns file size in a formatted string (i.e. 706 MiB)"""
+    return '{} {}'.format(
+        *regex_match(
+            r'^.*?(?=(\d+.?\d*)).+?(?:(B|KiB|MiB|GiB|MB|GB))', size).groups()
+    )
+
+
+def format_file_size_to_int(size: str) -> int:
+    """Returns file size in an integer value (i.e. 706 MiB => 740294656)"""
+    return parse_size(size)
+
+
+def get_first_element(html: Union[Element, HTML], locator: str) -> Element:
+    """Wrapping the find method of the HTML object"""
+    return html.find(selector=locator, first=True)
+
+
+def get_user_input() -> str:
+    """Get search query from user by command line or prompt"""
+    try:
+        return argv[1]
+    except IndexError:
+        return input('Enter search query:\n')
+
+
+def parse_comments(html: HTML, locator: str) -> List[Element]:
+    """Return list of comments from page."""
+    return html.find(selector=locator)
+
+
+def print_border(name: str = None):
+    """Print simple border to divide up output,
+    if name print bottom border out too
+    """
+    print(158*'*')
+    if name:
+        print(f'{70*"#"}{name}{70*"#"}')
+        print(158*"*")
+
+
+def save_page(html: HTML, file_name: str = "test.html"):
+    """Helper function to save page as an html file."""
+    with open(file_name, "w") as f:
+        f.write(html.html)
+
+
+class RequestsHtmlWrapper:
+    """A Wrapper class for the request_html library.
+
+    Instances are generated when passing in a valid url then a request is made
+    with that url and then the response attribute of the instance is set.
+
+    This wrapper allows a user to make a request with a url and access the
+    response object. In addition to accessing a response object with an
+    instance, one is also able to access and parse the HTML object.
+
+    Examples:
+        URL is used to instantiate the class
+        request_wrapper = HtmlRequestWrapper("https://www.example.com/")
+
+        Use HTML tag with call function to get list of elements from HTML
+        anchor_elements = request_wrapper("a")
+
+        Access Requests response methods and attributes
+        status_code = request_wrapper.response.status_code
+
+        Access HTML object for the current response with the .html property
+        html = request_wrapper.html
+
+        Search for elements within the HTML object
+        web_element = request_wrapper.html.find(locator)
+    """
+
+    def __init__(self, url: str, agent: str = 'Mozilla/5.0'):
+        self.session: HTMLSession = HTMLSession()
+        self.headers: dict = {
+            'Content-Type': 'text/plain; charset=UTF-8',
+            'User-Agent': agent
+        }
+        self.url: str = url
+        self.cookies = None
+        self.response: HTMLResponse = self.__response
+        self.html: HTML = self.response.html
+
+    def __repr__(self) -> str:
+        return f'<Request_HTML_Wrapper url={self.url}>'
+
+    def __call__(self, tag_name) -> List[Element]:
+        """Shortcut method for HTMLResponse.html.find just like BeautifulSoup
+
+        Returns:
+            A list of elements for matching tag
+        """
+        return self.html.find(selector=tag_name)
+
+    @property
+    @retry(wait_fixed=500, stop_max_attempt_number=5)
+    def __response(self) -> HTMLResponse:
+        """Return HTMLResponse with the instance's url."""
+        response: HTMLResponse = self.session.get(
+            url=self.url, headers=self.headers, cookies=self.cookies
+        )
+        response.raise_for_status()
+        return response
+
+    def get_page(self, page_url: str) -> HTML:
+        """Make request with given url and return html"""
+        response: HTMLResponse = self.session.get(
+            url=page_url, headers=self.headers, cookies=self.cookies
+        )
+        return response.html
